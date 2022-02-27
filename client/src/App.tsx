1d7e34a3cfd8d3e59412dfb7464c3bc711d88cc5
ryan <ryanhumphrey777@gmail.com>
2022-02-26 19:36:04 -0500

    leaving build in to see if works

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import People from './people/people';
+
+function App() {
+  return (
+    <div className="App">
+      <People/>
+    </div>
+  );
+}
+
+export default App;
