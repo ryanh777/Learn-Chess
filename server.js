6cb36abbe0ae489fa4978be19420d22dacef5345
ryan <ryanhumphrey777@gmail.com>
2022-02-26 22:43:48 -0500

    changing postbuild script

--- server.js
+++ server.js
@@ -6,22 +6,28 @@ require('dotenv/config')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Data parsing
 app.use(express.json());
 app.use(express.urlencoded({
+    // Allows nested objects
     extended: true
 }));
 
+// Routes
 // app.use('/', require('./routes/index'))
 app.use('/api', require('./routes/api'))
 
+// Connect to database
 mongoose.connect(process.env.MONGODB_URI || process.env.DB_CONNECTION)
 
 console.log("NODE_ENV:", process.env.NODE_ENV)
-// if (process.env.NODE_ENV === 'production') {
+
+// Serves the client build folder to server
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
-// }
+}
 
 app.listen(PORT, () => console.log('Server started on port', PORT))
